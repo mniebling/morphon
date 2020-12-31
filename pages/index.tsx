db925242a1b9f7936a770c57624952757a7422df
Mike Niebling <michael.niebling@gmail.com>
2020-12-31 15:59:51 -0600

    Accept files and show their filenames

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,105 +1,45 @@
 import Head from 'next/head'
-import { useEffect } from 'react'
-import { attachDropHandlers } from '../components/drag-and-drop'
+import React, { useEffect, useState } from 'react'
+import css from './index.module.css'
 
 
-export default function Home() {
+export default function Home () {
+
+  const [files, setFiles] = useState<File[]>([])
 
   useEffect(() => {
-    attachDropHandlers()
+    window.addEventListener('drop', (event) => {
+      event.preventDefault()
+      event.stopPropagation()
+
+      if (!event.dataTransfer) return
+
+      Array.from(event.dataTransfer.files).forEach(file => {
+        console.info(file)
+      })
+
+      setFiles(Array.from(event.dataTransfer.files))
+    })
+
+    // If you want to allow a drop, you must prevent the default handling by cancelling both the dragenter and dragover events.
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#Specifying_Drop_Targets
+    window.addEventListener('dragenter', (event) => event.preventDefault())
+    window.addEventListener('dragover', (event) => event.preventDefault())
   })
 
   return (
-    <div className="container">
+    <div className={ css.container }>
       <Head>
-        <title>Create Next App</title>
+        <title>Morphon</title>
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div className="test">
+        <h1>Start by dropping files in this window</h1>
 
-        <p className="description" id="drag">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+        { files.map((file, i) => (
+          <div key={ i }>{ file.name }</div>
+        ))}
+      </div>
     </div>
   )
 }
